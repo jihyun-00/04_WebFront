33572555500e36762940d590631171055ded5aa1
jihyun-00 <rkdwl811@naver.com>
2025-04-03 00:51:21 +0900

    분식집 메뉴판 완성

--- 3_Javascript/js/메뉴판관리.js
+++ 3_Javascript/js/메뉴판관리.js
@@ -18,11 +18,26 @@ document.querySelector("#updateBtn").addEventListener("click", () => {
     input1.type = "text";
     input1.classList.add("menu-name-input");
 
+
+    if (menuName.innerText == "미입력") {
+
+      input1.value = "";
+      input1.placeholder = "메뉴명";
+
+    } else {
+      input1.value = menuName.innerText;
+
+    }
+
+
     menuName.after(input1);
 
     menuName.style.display = "none";
     input1.style.display = "block";
-    input1.value = menuName.innerText;
+
+
+
+
 
   }
 
@@ -36,14 +51,27 @@ document.querySelector("#updateBtn").addEventListener("click", () => {
     input2.type = "text";
     input2.classList.add("menu-price-input");
 
+    const regExp = /^\d+원/;
+    if (menuPrice.innerText == "0원") {
+
+      input2.value = "";
+      input2.placeholder = "가격";
+
+    } else if (regExp.test(menuPrice.innerText)) {  // test()를 사용해 문자열이 패턴과 일치하는지 확인
+      input2.value = menuPrice.innerText.match(/\d+/)[0];  // 숫자 부분만 추출
+
+    } else {
+      input2.value = menuPrice.innerText;
+
+    }
+
     menuPrice.after(input2);
 
     menuPrice.style.display = "none";
     input2.style.display = "block";
-    input2.value = menuPrice.innerText;
+
 
   }
-
 });
 
 // 추가 버튼 눌렀을 때 기능
@@ -109,7 +137,7 @@ document.querySelector("#deleteMenu").addEventListener("click", () => {
 // 종료 버튼 눌렀을 때 기능
 document.querySelector("#exitBtn").addEventListener("click", () => {
 
-  document.querySelector("#updateBtn").style.display = "block";
+  document.querySelector("#updateBtn").style.display = "inline";
   document.querySelector(".b-hidden").style.display = "none";
 
   const checkboxs = document.querySelectorAll(".check");
@@ -158,6 +186,13 @@ document.querySelector("#exitBtn").addEventListener("click", () => {
 
     if (price.nextElementSibling.value.length == 0) {
       price.innerText = "0원";
+    }
+
+    const regExp = /^\d+/;
+
+    if (regExp.test(price.nextElementSibling.value)) {  // test()를 사용해 문자열이 패턴과 일치하는지 확인
+      price.innerText = price.nextElementSibling.value + "원";  // 숫자 부분만 추출
+
     }
   }
 
